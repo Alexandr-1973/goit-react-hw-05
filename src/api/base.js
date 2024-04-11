565b1e17714b738ee8eb84c80ad013d61c5ba474
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-04-11 16:10:02 +0300

    baseserver

---
+++ src/api/base.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://api.themoviedb.org";
+const SEARCH_HEADER = "/3/";
+const ApiKey = "289d4e60f2dcffe7ad4eaad1d3bf609e";
+const ApiReadAccessToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODlkNGU2MGYyZGNmZmU3YWQ0ZWFhZDFkM2JmNjA5ZSIsInN1YiI6IjY2MTdkOTU2OTBiODdlMDE2MzNkN2E5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dGo5ChXC_3B_OFoMudf4VDWpWDLePN92e3q3EjOju1I";
+//   const PER_PAGE = 12;
+//   const url = `${BASE_URL}${SEARCH_HEADER}`;
+//   const params = {
+//     client_id: Client_ID,
+//     query: topic,
+//     per_page: PER_PAGE,
+//     page: page,
+//   };
+//   const res = await axios.get(url, { params });
+//   return res.data;
